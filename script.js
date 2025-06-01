ecfd8a6bd7aaad30f19df035014419881fa496b5
わたあめえ <Wataamee777@gmail.com>
2025-06-01 17:30:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function KaihatuLanguage() {
   const siteList = document.getElementById("KaihatuLanguageList");
   siteList.classList.toggle("hidden");
 }
+let tapCount = 0;
+const icon = document.getElementById("main-icon");
+const secretMessage = document.getElementById("secret-message");
+
+icon.addEventListener("click", () => {
+  tapCount++;
+  if (tapCount === 3) {
+    // 3回目タップ時の処理
+    icon.classList.add("glow-effect");
+    secretMessage.classList.remove("hidden");
+
+    // 5秒後に光りとメッセージを消す
+    setTimeout(() => {
+      icon.classList.remove("glow-effect");
+      secretMessage.classList.add("hidden");
+      tapCount = 0; // カウントリセット
+    }, 5000);
+  }
+});
