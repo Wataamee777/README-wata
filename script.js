27e322ea23fdc10f1c8896a90e6d266d43dab575
わたあめえ <Wataamee777@gmail.com>
2025-06-01 17:27:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ function toggleSites() {
   const siteList = document.getElementById("siteList");
   siteList.classList.toggle("hidden");
 }
+function KaihatuLanguage() {
+  const siteList = document.getElementById("KaihatuLanguageList");
+  siteList.classList.toggle("hidden");
+}
